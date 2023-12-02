71f53006811ad5f251b5d69693f027745f9c15b1
enigmaticthor <enigmaticthor@gmail.com>
2023-12-02 09:22:38 -0800

    Day 1 part 2 solution

--- code/day1.py
+++ code/day1.py
@@ -4,16 +4,40 @@ for line in file:
     line = line.strip('\n')
     calibration.append(line)
 file.close()
-
-def number_catcher(test):
-    return test.isnumeric()
+text_numbers = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+def number_catcher(line):
+    line_number_list = []
+    line_characters = ''
+    #take any numeric numbers and throw them in an array
+    for x in line:
+        if x.isnumeric():
+            line_number_list.append(x)
+            line_characters = ''
+        else:
+            #grab characters one at a time until there's a match, then remove all but the last character
+            line_characters = line_characters + x
+            for key, value in text_numbers.items():
+                if key in line_characters:
+                    line_number_list.append(value)
+                    line_characters = line_characters[-1]
+                    break
+            print(line_characters)
+    return line_number_list
+    
 
 numbers = []
 for line in calibration:
-    line_numbers = []
-    for x in line:
-        if x.isnumeric():
-            line_numbers.append(x)
+    line_numbers = number_catcher(line)
     line_number = line_numbers[0] + line_numbers[-1]
     numbers.append(int(line_number))
 print(sum(numbers))
