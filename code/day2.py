300a9fe8b48f6dbe33f88e28755b939eb440c156
enigmaticthor <enigmaticthor@gmail.com>
2023-12-03 15:32:01 -0800

    Day 2 pt 1 solution

--- code/day2.py
+++ code/day2.py
@@ -1,7 +1,13 @@
 from pathlib import Path
-input_folder = Path("../input")
+input_folder = Path("input")
 input_file = input_folder / "day2.txt"
 file = open(input_file,"r")
+#total numbers of valid cubes
+valid_criteria = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
 games = {}
 i = 0
 for line in file:
@@ -11,21 +17,46 @@ for line in file:
     if i < 10:
         number = line[:2]
         game = line[3:]
-    elif i > 10 and i < 100:
+    elif i >= 10 and i < 100:
         number = line[:3]
         game = line[4:]
     else:
         number = line[:4]
         game = line[5:]
-    game = game.strip(': ')
+    game = game.strip(':').strip()
+    game = game.split(';')
+    sets = []
+    for set in game:
+        sets.append(set.strip().split(', '))
     number = number.strip(': ')
-    games[number] = game
+    games[number] = sets
 file.close()
 valid_games = []
-#total numbers of valid cubes
-valid_criteria = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}   
-print(games)
+invalid_games = []
+for game_id, game_sets in games.items():
+    valid = True
+    for pull in game_sets:
+        if valid == False:
+            break
+        for color in pull:
+            if valid == False:
+                break
+            color_split = color.split(' ')
+            if "red" in color_split[1]:
+                if int(color_split[0]) > 12:
+                    valid = False
+                    break
+            elif "green" in color_split[1]:
+                if int(color_split[0]) > 13:
+                    valid = False
+                    break
+            elif "blue" in color_split[1]:
+                if int(color_split[0]) > 14:
+                    valid = False
+                    break
+
+    if valid == True:
+        valid_games.append(int(game_id))
+    else:
+        invalid_games.append(int(game_id))
+print(sum(valid_games))
