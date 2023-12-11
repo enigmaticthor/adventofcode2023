dac10da2fde55ca8306068f5b7d6e73349ac1b9c
enigmaticthor <enigmaticthor@gmail.com>
2023-12-10 22:31:42 -0800

    day 2 part 2 finally.

--- code/day2.py
+++ code/day2.py
@@ -49,11 +49,32 @@ def get_validity(color):
     return valid
 
 def get_set_power(game_sets):
-    set_value = ''
-    return set_value
+    highest_for_color = {
+        "red": 0,
+        "blue": 0,
+        "green": 0,
+    }
+    for cube_pull in game_sets:
+        for color in cube_pull:
+            color_split = color.split(' ')
+            if "red" in color_split[1]:
+                if int(color_split[0]) > highest_for_color["red"]:
+                    highest_for_color["red"] = int(color_split[0])
+            elif "green" in color_split[1]:
+                if int(color_split[0]) > highest_for_color["green"]:
+                    highest_for_color["green"] = int(color_split[0])
+            elif "blue" in color_split[1]:
+                if int(color_split[0]) > highest_for_color["blue"]:
+                    highest_for_color["blue"] = int(color_split[0])
+    power = 1
+    for color in highest_for_color:
+        power = power*highest_for_color[color]
+    return power
 
+game_values = []
 for game_id, game_sets in games.items():
     valid = True
+    game_values.append(get_set_power(game_sets))
     for pull in game_sets:
         if valid == False:
             break
@@ -66,4 +87,7 @@ for game_id, game_sets in games.items():
     else:
         invalid_games.append(int(game_id))
 
-print(sum(valid_games))
+print("Part 1: ")
+print(sum(valid_games)) #part 1 solution
+print("Part 2:")
+print(sum(game_values)) #part 2 solution
